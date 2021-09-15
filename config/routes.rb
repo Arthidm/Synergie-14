378b37c1f278ca67797fae8195f3dc017f1dadb1
Arthur De Meyer <arthur.demeyer75@gmail.com>
2021-09-15 13:42:56 +0200

    pray

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'connaitre', to: 'pages#connaitre', as: 'connaitre'
+  get 'soutenir', to: 'pages#soutenir', as: 'soutenir'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  get 'impact', to: 'pages#impact', as: 'impact'
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
