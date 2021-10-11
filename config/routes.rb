b7b34cffb775f406234ef90cb0c60d4005fc73ff
Arthur De Meyer <arthur.demeyer75@gmail.com>
2021-10-11 21:26:01 +0200

    third wave

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "home/download"
   get 'connaitre', to: 'pages#connaitre', as: 'connaitre'
   get 'soutenir', to: 'pages#soutenir', as: 'soutenir'
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'impact', to: 'pages#impact', as: 'impact'
 
-
-
-
+  match 'download', to: 'pages#download', as: 'download', via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
