61637f84c82b189e10dfb7581a4a7e27e6864d6a
Arthur De Meyer <arthur.demeyer75@gmail.com>
2021-10-22 13:15:19 +0200

    4th wave

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'soutenir', to: 'pages#soutenir', as: 'soutenir'
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'impact', to: 'pages#impact', as: 'impact'
+  get 'test', to: 'pages#test', as: 'test'
+
 
   match 'download', to: 'pages#download', as: 'download', via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
